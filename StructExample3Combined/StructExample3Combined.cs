15a625f19c76337e642bce62f3645cc007857617
Michael Herman (Lenovo) <mwherman@parallelspace.net>
2018-02-14 08:37:51 -0500

    Fixes

--- StructExample3Combined/StructExample3Combined.cs
+++ StructExample3Combined/StructExample3Combined.cs
@@ -1,6 +1,4 @@
 ﻿using Neo.SmartContract.Framework;
-using System;
-using System.Numerics;
 
 namespace StructExample
 {
@@ -49,6 +47,49 @@ namespace StructExample
             p.X = x;
             p.Y = y;
             return p;
+        }
+    }
+
+    public class StructExample3Combined : SmartContract
+    {
+        private static Point2 Add(Point2 a, Point2 b)
+        {
+            return new Point2
+            {
+                X = a.X + b.X,
+                Y = a.Y + b.Y
+            };
+        }
+
+        public static Point2 Main()
+        {
+            Point2 p1 = new Point2
+            {
+                X = 1,
+                Y = 2
+            };
+            Point2 p2 = new Point2
+            {
+                X = 2,
+                Y = 1
+            };
+
+            Point2[] array = new[]
+            {
+                p1, p2
+            };
+
+            Point2 p3 = Add(array[0], array[1]);
+
+            Point2.Put("p1", p1);
+            Point2.Put("p2", p2);
+            Point2.Put("p3", p3);
+
+            Point2 p1get = Point2.Get("p1");
+            Point2 p2get = Point2.Get("p2");
+            Point2 p3get = Point2.Get("p3");
+
+            return p3get;
         }
     }
 }
