8256b9ae7c7a3c2870edd6048bc289abcea52d0b
Michael Herman (Toronto) <mwherman@parallelspace.net>
2018-02-27 12:04:26 -0500

    Added String1

---
+++ Strings1/Contract1.cs
@@ -0,0 +1,30 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace Strings1
+{
+    public class Contract1 : SmartContract
+    {
+        public static string Main()
+        {
+            string message = "";
+
+            //string empty = "";
+            //string nullstring = null;
+
+            //if (String.IsNullOrWhiteSpace(empty)) message += "String.IsNullOrWhiteSpace(empty) worked. ";
+
+            //if (String.IsNullOrEmpty(empty)) message += "String.IsNullOrEmpty(empty) worked. ";
+
+            //if (String.IsNullOrEmpty(nullstring)) message += "String.IsNullOrEmpty(nullstring) worked. ";
+
+            string nullstring = null;
+            if (nullstring == null) message += "nullstring == null worked. ";
+            nullstring = "Hello World";
+            message += nullstring;
+            return message;
+        }
+    }
+}
