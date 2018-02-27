a9806e5750b23e427bfe968e01adf65ab8763544
Michael Herman (Toronto) <mwherman@parallelspace.net>
2018-02-27 15:07:34 -0500

    Book1

---
+++ Book1/Contract1.cs
@@ -0,0 +1,27 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace Book1
+{
+    public class Contract1 : SmartContract
+    {
+        public struct Book
+        {
+            public decimal price;
+            public string title;
+            public string author;
+        }
+
+        public static object Main()
+        {
+            Book b = new Book();
+            b.author = "Douglas Adams";
+            b.title = "HHGTTG";
+            Storage.Put(Storage.CurrentContext, "testkey", b);
+            byte[] book = Storage.Get(Storage.CurrentContext, "testkey");
+            return b; // or return book;
+        }
+    }
+}
