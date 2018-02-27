c9eeb651c5def50ad04a256e61499f9bf693467f
Michael Herman (Toronto) <mwherman@parallelspace.net>
2018-02-27 17:17:59 -0500

    More samples

---
+++ Strings2/Contract1.cs
@@ -0,0 +1,17 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace Strings2
+{
+    public class Contract1 : SmartContract
+    {
+        public static void Main()
+        {
+            string str = "Hello";
+            str += " There";
+            Runtime.Notify(str);
+        }
+    }
+}
