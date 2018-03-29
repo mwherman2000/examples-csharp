f80bb0913853ef33a283fef96a7424d9e3ce69e8
Michael Herman (Toronto) <mwherman@parallelspace.net>
2018-03-29 00:28:05 -0400

    Updates

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,4 +1,5 @@
 ﻿using Neo.SmartContract.Framework.Services.Neo;
+using System;
 
 namespace Neo.SmartContract
 {
@@ -7,6 +8,37 @@ namespace Neo.SmartContract
         public static void Main()
         {
             Storage.Put(Storage.CurrentContext, "Hello", "World");
+
+            int count = 1;
+            Runtime.Notify("counta", count);
+
+            Runtime.Notify("countb", count);
+            count = 2;
+            Runtime.Notify("countc", count);
+
+            doSomething();
+
+            if (true) goto TheEnd;
+
+            Runtime.Notify("countd", count);
+            count = 3;
+            Runtime.Notify("counte", count);
+            Runtime.Notify("countf", count);
+            count = 4;
+            Runtime.Notify("countg", count);
+
+            Runtime.Notify("counth", count);
+            count = 5;
+            Runtime.Notify("counti", count);
+
+            TheEnd: return;
+        }
+
+        private static void doSomething()
+        {
+            //goto TheEnd;
+            //Exit();
+            //Environment.Exit(0);
         }
     }
 }
